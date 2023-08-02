5929080bbede567c1df053776b1b6fc84249e933
MkIINice <Mk1Hitch@gmail.com>
2023-08-02 22:59:10 +0300

    add destroy service

--- src/app/dashboard/newsModule/news/news.component.ts
+++ src/app/dashboard/newsModule/news/news.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component,OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs';
 import { IArticle } from 'src/app/shared/interfaces/article.interface';
 import { IResponse } from 'src/app/shared/interfaces/response.interface';
+import { DestroyService } from 'src/app/shared/services/destroy.service';
 import { NewsApiService } from 'src/app/shared/services/news-api.service';
 
 @Component({
@@ -9,18 +11,20 @@ import { NewsApiService } from 'src/app/shared/services/news-api.service';
   styleUrls: ['./news.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewsComponent implements OnInit{
+export class NewsComponent implements OnInit {
+  public articles: IArticle[] = [];
 
-  public articles: IArticle[] = []
-
-  constructor(private newsApiService: NewsApiService, private cdr: ChangeDetectorRef){}
+  constructor(private newsApiService: NewsApiService, private cdr: ChangeDetectorRef, private destroy$: DestroyService,) { }
 
   public ngOnInit(): void {
-    this.newsApiService.getArticle().subscribe((data: IResponse)=>{
-      console.log(data);
-      this.articles = data.results;
-      this.cdr.detectChanges()
-    });
+    this.newsApiService.getArticle()
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe((data: IResponse) => {
+        this.articles = data.results;
+        this.cdr.detectChanges();
+      });
 
   }
 }
